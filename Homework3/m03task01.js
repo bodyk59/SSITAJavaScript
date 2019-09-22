ccd9e1eacd0418060c835c6dea6ef2aa9e864028
bodyk59 <bogdan.kurchak@icloud.com>
2019-09-23 02:34:16 +0300

    Homework 3: Data collection and Program flow

---
+++ Homework3/m03task01.js
@@ -0,0 +1,24 @@
+﻿// Task01 - create a function with name task01
+// function pass three numbers and multiples those that less than zero
+// if all numbers are positive function should return undefined
+// for example: 
+// if function take 3, -2 and -5 it should return 10
+
+function task01(number1, number2, number3) {
+    if ((number1 > 0) && (number2 > 0) && (number3 > 0)){
+        return undefined;
+    }
+
+    var result = 1;
+
+    if (number1 < 0){
+        result *= number1;
+    }
+    if (number2 < 0){
+        result *= number2;
+    }
+    if (number3 < 0){
+        result *= number3;
+    }
+    console.log(result);
+}
---
+++ Homework3/m03task03.js
@@ -0,0 +1,32 @@
+﻿// Task03 - create a function with name task03
+// function pass array and retun hash with min amd max values from array
+// for example: 
+// if function take [-1, 8, -3, 0, 7] it should return {min: -3, max: 8}
+
+function task03(array) {
+    var hash_table = {
+        min: "",
+        max: ""
+    };
+
+    var max = array[0];
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] > max){
+            max = array[i];
+        }
+    }
+
+    var min = array[0];
+
+    for (i = 1; i < array.length; i++) {
+        if (array[i] < min){
+            min = array[i];
+        }
+    }
+
+    hash_table["max"] += max;
+    hash_table["min"] += min;
+
+    console.log(hash_table);
+}
