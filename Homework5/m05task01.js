6aa2b739bda9b05d4366cf927f2580ef3e23a179
bodyk59 <bogdan.kurchak@icloud.com>
2019-09-24 19:31:04 +0300

    Homework 5: JavaScript in Browser

---
+++ Homework5/m05task01.js
@@ -0,0 +1,17 @@
+﻿// Task01 - Create a simple function that will be used as event handler.
+// function should cancel event bubbling
+window.addEventListener("load", task01, false);
+
+function task01() {
+    button.addEventListener("click", action, false);
+}
+function action() {
+    var cancel = false,
+        answer = confirm("Current phase bubbling. Do you want continue?");
+    if (answer === cancel){
+        button.stopPropagation();
+    }
+}
+
+
+
