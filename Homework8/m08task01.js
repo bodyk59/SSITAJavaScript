cc76c38cf3d9d61496e3156e8e486a3056511108
bodyk59 <bogdan.kurchak@icloud.com>
2019-09-26 02:14:23 +0300

    Homework 8: RegExp

---
+++ Homework8/m08task01.js
@@ -0,0 +1,15 @@
+﻿// Task01 - create regular expression which tests for corectness phone numbers.
+// Phone number should have 10 digits optionally divided to 3-digit groups by periods ("."), dashes ("-"),
+// brackets ("()"), or space symbols (" "). Brackets should be used only at the beginning
+// Examples of correct phone numbers:
+// (123) 456 7899
+// (123).456.7899
+// (123)-456-7899
+// 123-456-7899
+// 123 456 7899
+// 1234567899
+
+function task01(testNumber) {
+    var reg = /\(?[0-9]{3}\)?\.?-?\s?[0-9]{3}\.?-?\s?[0-9]{4}/gm;
+   console.log(reg.test(testNumber));
+}
