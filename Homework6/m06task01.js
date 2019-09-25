2001f5a22906b76a867c46761991945cff49bf7e
bodyk59 <bogdan.kurchak@icloud.com>
2019-09-25 13:47:14 +0300

    Homework6: Troubleshoot JavaScript code

---
+++ Homework6/m06task01.js
@@ -0,0 +1,9 @@
+﻿// Task01 
+// Your task is to check if argument is undefined and if it is true,
+// throw an exception of type Error with message 'missing parameter'
+
+function m06task01(arg) {
+  if (arg === undefined){
+      throw (new Error("missing parameter"))
+  }
+}
---
+++ Homework6/m06task02.js
@@ -0,0 +1,18 @@
+﻿// Task02
+// Your task is to create function that returns square number of an argument
+// Function should check if argument is incorrect and return two different
+// exceptions as instances of Error object:
+// 'missing parameter' if argument is not initialized
+// 'parameter is not a number' if argument is Nan
+
+function m06task02(arg) {
+    if (arg === undefined){
+        throw (new Error('missing parameter'));
+    } else if(isNaN(arg)){
+        throw (new Error('parameter is not a number'));
+        //One more variant:
+        // console.assert(!isNaN(arg), 'parameter is not a number');
+    } else{
+        return arg * arg;
+    }
+}
