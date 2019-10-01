6729920058849b9fcfbfe42d9829dfdf8070323c
bodyk59 <bogdan.kurchak@icloud.com>
2019-10-01 17:13:37 +0300

    Homework 10: Ajax

---
+++ Homework10/m10task01.js
@@ -0,0 +1,49 @@
+﻿// Task01 
+// Create a function that makes Ajax request to URL passed as parameter
+// Server returns data in JSON format, ready property "name" of data file
+// and write it to span with ID "target" of a document "target.html"
+// Note: function may use jQuery
+
+// Arguments:
+// Function accepts one argument, a string with URL to request
+
+// Return result:
+// Function should not return any data, it should write to span element 
+// with ID "target"
+
+function m10task01(targetURL) {
+    //1. Using JavaScript to make Ajax Requests:
+
+    // function getXHR() {
+    //     var xhr;
+    //     if (window.XMLHttpRequest){
+    //         xhr = new XMLHttpRequest();
+    //     } else{
+    //         xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    //     }
+    //     return xhr;
+    // }
+    //
+    // var xhr = getXHR();
+    // xhr.onreadystatechange = function () {
+    //     if (xhr.readyState === 4 && xhr.status === 200){
+    //         var clientData = JSON.parse(xhr.responseText);
+    //         document.getElementById("target").innerHTML = clientData.name;
+    //     }
+    // };
+    //
+    // xhr.open("GET", targetURL, true);
+    // xhr.send();
+
+
+    // 2. Using jQuery to make Ajax Requests:
+
+    $.ajax({
+        type: 'GET',
+        url: targetURL,
+        dataType: 'json',
+        success: function (data) {
+            $('#target').html(data.name);
+        }
+    })
+}
---
+++ Homework10/m10task02.js
@@ -0,0 +1,42 @@
+﻿// Task02
+// Create a function that returns ZIP code of any area code using 
+// Yahoo! service: https://developer.yahoo.com/yql/
+// Note: function should make JSONP request and may use jQuery
+// See examples to this module for reference
+
+// Arguments:
+// Function accepts one argument, a string with area code
+
+// Return result:
+// Function should not return any data, it should replace inner 
+// contents of an element with id "target" on a file "target.html"
+
+// Sample:
+// argument value: SFO
+// return result written: 94128 
+
+function m10task02(code) {
+    // 1. Using .getJSON method:
+
+    // $.getJSON("http://query.yahooapis.com/v1/public/yql?callback=?",
+    //     {q: "SELECT from geo.places WHERE text = " + code, format: "json"},
+    //     function(response) {
+    //         $('#target').html(response.query.results.place.postal.content);
+    //     })
+
+
+    // 2. Using .ajax method:
+
+    $.ajax({
+        url: "http://query.yahooapis.com/v1/public/yql",
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            q: "SELECT from geo.places WHERE text = " + code,
+            format: "json"
+        },
+        success: function (response) {
+            $("#target").html(response.query.results.place.postal.content);
+        }
+    })
+}
